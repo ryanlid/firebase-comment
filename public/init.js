f9bba20e1ee3804975c0c4eca46bc09e88ad1af0
OOnnnOO <hi@lidong.me>
2017-03-14 21:18:59 +0800

    first commit :rocket:

---
+++ public/init.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBXc1jXxgQXzFepU363uINZf4IfV7OGRis",
+    authDomain: "hidden-cosmos-602.firebaseapp.com",
+    databaseURL: "https://hidden-cosmos-602.firebaseio.com",
+    storageBucket: "hidden-cosmos-602.appspot.com",
+    messagingSenderId: "593403326228"
+};
+firebase.initializeApp(config);
